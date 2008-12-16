e5d772bb228251559d1012c83b1bad358c77ed9a
ringmaster <USER@DOMAIN>
2008-12-16 17:58:40 +0000

    Coding standards, typos.
    git-svn-id: http://svn.habariproject.org/habari-extras/plugins/sharedraft/trunk@1466 f4a90942-1786-4aa1-b092-57c31935ddc3

--- sharedraft.plugin.php
+++ sharedraft.plugin.php
@@ -1,6 +1,6 @@
 <?php
 
-class ShardraftPlugin extends Plugin
+class ShareDraftPlugin extends Plugin
 {
 	/**
 	 * function info
@@ -14,17 +14,23 @@ class ShardraftPlugin extends Plugin
 			'url' => 'http://habariproject.org/',
 			'author' => 'Owen Winkler',
 			'authorurl' => 'http://asymptomatic.net/',
-			'version' => '1.0',
+			'version' => '1.0.1',
 			'description' => 'Allows an author to share a draft of a post using a special link',
 			'license' => 'Apache License 2.0',
 		);
 	}
 
+	/**
+	 * Update the where filters for querying a post if the sharedraft key is set
+	 *
+	 * @param array $filters The array of pre-existing filters
+	 * @return array The modified array, if the key is set
+	 */
 	function filter_template_where_filters($filters)
 	{
-		if(isset($_GET['showdraft']) && isset($filters['status']) && $filters['status'] == Post::status('published')) {
+		if(isset($_GET['sharedraft']) && isset($filters['status']) && $filters['status'] == Post::status('published')) {
 			$slug = $filters['slug'];
-			$key = $_GET['showdraft'];
+			$key = $_GET['sharedraft'];
 			if($key == md5($slug . Options::get('guid'))) {
 				$filters['status'] = Post::status( 'any' );
 			}
@@ -32,11 +38,17 @@ class ShardraftPlugin extends Plugin
 		return $filters;
 	}
 
+	/**
+	 * Update the publish form  to display the draft link
+	 *
+	 * @param FormUI $form The publishing form
+	 * @param Post $post The post displayed in the form
+	 */
 	function action_form_publish($form, $post)
 	{
 		if($post->slug != '' && $post->status == Post::status('draft')) {
 			$key = md5($post->slug . Options::get('guid'));
-			$url = "{$post->permalink}?showdraft={$key}";
+			$url = "{$post->permalink}?sharedraft={$key}";
 			$notice = <<< NOTICE
 <div class="container formcontrol transparent">
 <p class="pct25"><label for="newslug">Share This Post:</label></p>
